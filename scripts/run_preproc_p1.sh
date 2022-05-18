1f2d25b624018d7154f2eea976c47511e31846bc
Tiantian HE <tiantihe@wise.cs.ucl.ac.uk>
2022-05-18 03:21:59 +0100

    split act_adni_1_preprocess into two parts because freesurfer segmentatneed to be run between those two parts; update some commands to suit the latest versions of MRtrix3; altered some file names; please see the comments in the .py files

---
+++ scripts/run_preproc_p1.sh
@@ -0,0 +1,30 @@
+#$ -l tmem=10G
+#$ -l h_vmem=10G
+#$ -l h_rt=10:00:0
+#$ -S /bin/bash
+#$ -N csh_preproc
+#$ -j y
+
+hostname
+date
+
+source /share/apps/source_files/conda.source
+conda activate lilypad_env #py2.7_mrtrix_env 
+export PATH=${PATH}:/share/apps/mrtrix3/bin
+export PATH=${PATH}:/share/apps/cmic/niftyreg_v1.5.43/bin
+source /share/apps/source_files/cuda/cuda-10.1.source
+export PATH=${PATH}:/share/apps/cmic/fsl-5.0.10/bin
+FSLDIR=/share/apps/cmic/fsl-5.0.10
+. ${FSLDIR}/etc/fslconf/fsl.sh
+PATH=${FSLDIR}/bin:${PATH}
+export FSLDIR PATH
+export PATH=${PATH}:/share/apps/ants-2.2.0/bin
+export PATH=${PATH}:/share/apps/freesurfer-6.0.0/bin
+export FREESURFER_HOME=/share/apps/freesurfer-6.0.0
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+export SUBJECTS_DIR=/cluster/project9/HCP_subjects/anatomically_constrained_tractography/ADNI/act
+
+cd /cluster/project9/HCP_subjects/anatomically_constrained_tractography/scripts
+
+python act_adni_p1_preprocess.py
+date
